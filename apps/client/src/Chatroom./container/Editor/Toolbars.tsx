aaf535070ee284dfd20960834048b44a5a8420cf
sumin <suminkim.me@gmail.com>
2023-12-30 15:58:59 +0900

    fix: svg 아이콘을 하나의 파일에서 export 처리

--- apps/client/src/Chatroom./container/Editor/Toolbars.tsx
+++ apps/client/src/Chatroom./container/Editor/Toolbars.tsx
@@ -3,11 +3,6 @@ import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext
 import { $getSelection, $isRangeSelection, FORMAT_TEXT_COMMAND } from "lexical";
 import { $getNearestNodeOfType, mergeRegister } from "@lexical/utils";
 import { ListPlugin } from "@lexical/react/LexicalListPlugin";
-import BoldIcon from "../../../assets/icons/editor-bold.svg?react";
-import UnderlineIcon from "../../../assets/icons/editor-underline.svg?react";
-import ItalicIcon from "../../../assets/icons/editor-italic.svg?react";
-import ListIcon from "../../../assets/icons/editor-list_bullet.svg?react";
-
 import {
   $isListNode,
   INSERT_UNORDERED_LIST_COMMAND,
@@ -15,6 +10,7 @@ import {
   REMOVE_LIST_COMMAND,
 } from "@lexical/list";
 import { ListNodeTagType } from "@lexical/list/LexicalListNode";
+import { BoldIcon, ItalicIcon, ListIcon, UnderlineIcon } from "./icons";
 
 interface Props {
   isFocused: boolean;
