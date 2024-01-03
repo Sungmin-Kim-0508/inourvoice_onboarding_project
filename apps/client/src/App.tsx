fcafbad4d5b569f45bf486420539ddd5c0576501
Sungmin Kim <seongmin.kim082@gmail.com>
2024-01-03 17:44:50 +0900

    feature/사이드바 채널 리스트 추가

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { ChatRoom } from "./Chatroom./container";
+import { Sidebar } from "./Sidebar";
 
 function App() {
   return (
     <main className="w-full flex">
       <aside className="min-h-screen max-w-[260px] min-w-[260px] h-full">
-        {/* Sidebar */}
+        <Sidebar />
       </aside>
       <div className="bg-zinc-800 flex flex-col w-full border-l border-zinc-600">
         <header className="h-[48px] px-3 py-[10px] text-zinc-300 border-b border-zinc-600">
---
+++ apps/client/src/Sidebar/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Header() {
+  return (
+    <div className=" px-[10px] py-[2px] mb-3 text-white">
+      <span className="text-lg font-extrabold leading-7">In’OurVoice</span>
+    </div>
+  );
+}
